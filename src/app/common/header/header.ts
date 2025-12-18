5da185dc39d03da44b6407cd8b472bb6c76b3a4e
Kapil <sonikapil309@gmail.com>
2025-12-18 21:47:49 +0530

    Added css changes

--- src/app/common/header/header.ts
+++ src/app/common/header/header.ts
@@ -12,26 +12,13 @@ export class Header {
 
   public isScrolled: boolean = false;
   public isMenuOpen: boolean = false;
-  public isHomePage: boolean = false;
 
   @HostListener('window:scroll', [])
   onWindowScroll(): void {
     this.isScrolled = window.scrollY > 50;
   }
 
-  private route = inject(Router);
-
-  public routeToHome(): void {
-    this.route.navigate(["/"]);
-  }
-
-  constructor(private router: Router) {
-    this.router.events.pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe((event: NavigationEnd) => {
-        this.isHomePage = event.urlAfterRedirects === '/' || event.urlAfterRedirects === '';
-        console.log(this.isHomePage)
-      });
-  }
+  private router = inject(Router);
 
   public routeTo(route: string): void {
     this.router.navigate([route]);
