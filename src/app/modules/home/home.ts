0a98760f8c928d77b22f343e11a3f57e4b62eec9
Kapil <sonikapil309@gmail.com>
2025-12-20 19:23:07 +0530

    Added CSS changes

--- src/app/modules/home/home.ts
+++ src/app/modules/home/home.ts
@@ -146,7 +146,7 @@ Paawana is not just about food—it's about the experience. Guests often find th
   rooms: Room[] = [
     {
       name: 'Courtyard Double Room',
-      description: 'An elegant room, refined comfort, modern amenities, and a welcoming ambience.',
+      description: `Designed for comfort and calm, the Courtyard Double Room offers a peaceful stay infused with the charm of Mandawa's heritage. Thoughtfully furnished with beautiful vintage pieces from the region, the room reflects timeless Rajasthani elegance. Clean, spacious, and welcoming, it is ideal for couples or solo travelers seeking a cozy retreat after a day of exploration.`,
       image: 'assets/images/delux-room.png',
       bedrooms: 1,
       area: '24 sq. m',
@@ -154,7 +154,7 @@ Paawana is not just about food—it's about the experience. Guests often find th
     },
     {
       name: 'Heritage Twin Room',
-      description: 'A thoughtfully designed room with twin beds, offering comfort, privacy, and elegance.',
+      description: `The Heritage Twin Room blends practicality with old-world charm. Featuring two comfortable separate beds, this room is perfect for friends or companions traveling together. Soft lighting, handcrafted vintage furniture, and a soothing ambiance create a warm and restful environment while preserving the authentic character of this historic town.`,
       image: 'assets/images/twin-room.png',
       bedrooms: 2,
       area: '21 sq. m',
@@ -162,7 +162,7 @@ Paawana is not just about food—it's about the experience. Guests often find th
     },
     {
       name: 'Family Courtyard Suite',
-      description: 'A spacious room designed for families, blending comfort with thoughtful design.',
+      description: `Spacious, elegant, and thoughtfully designed, the Family Courtyard Suite is perfect for families or small groups. With two comfortable double beds and ample space to relax, the room combines modern comfort with traditional vintage furniture sourced from the region. Its warm interiors and refined details make it feel like a home rooted in heritage`,
       image: 'assets/images/family-room.png',
       bedrooms: 2,
       area: '64 sq. m',
