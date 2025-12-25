6d96ba0dc7e9588de5bf47b66c96a86d303f3fc0
Kapil <sonikapil309@gmail.com>
2025-12-25 09:39:48 +0530

    Added about us page changes.

--- src/app/modules/about/about.ts
+++ src/app/modules/about/about.ts
@@ -7,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrl: './about.css',
 })
 export class About {
+  rooms: Room[] = [
+    {
+      name: 'Courtyard Double Room',
+      description: `Designed for comfort and calm, the Courtyard Double Room offers a peaceful stay infused with the charm of Mandawa's heritage. Thoughtfully furnished with beautiful vintage pieces from the region, the room reflects timeless Rajasthani elegance. Clean, spacious, and welcoming, it is ideal for couples or solo travelers seeking a cozy retreat after a day of exploration.`,
+      image: 'assets/images/delux-room.png',
+      bedrooms: 1,
+      area: '24 sq. m',
+      price: 5000,
+      occupancy: 2
+    },
+    {
+      name: 'Heritage Twin Room',
+      description: `The Heritage Twin Room blends practicality with old-world charm. Featuring two comfortable separate beds, this room is perfect for friends or companions traveling together. Soft lighting, handcrafted vintage furniture, and a soothing ambiance create a warm and restful environment while preserving the authentic character of this historic town.`,
+      image: 'assets/images/twin-room.png',
+      bedrooms: 2,
+      area: '24 sq. m',
+      price: 5000,
+      occupancy: 2
+    },
+    {
+      name: 'Family Courtyard Suite',
+      description: `Spacious, elegant, and thoughtfully designed, the Family Courtyard Suite is perfect for families or small groups. With two comfortable double beds and ample space to relax, the room combines modern comfort with traditional vintage furniture sourced from the region. Its warm interiors and refined details make it feel like a home rooted in heritage`,
+      image: 'assets/images/family-room.png',
+      bedrooms: 2,
+      area: '64 sq. m',
+      price: 7000,
+      occupancy: 4
+    }
+  ];
+}
 
+export interface Room {
+  name: string;
+  description: string;
+  image: string;
+  bedrooms: number;
+  area: string;
+  price: number;
+  occupancy: number;
 }
